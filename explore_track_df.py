ca97941233fb05b9b98c821c5f6611bfb27daa1d
Soren Sabet Sarvestany <soren.sabet@gmail.com>
2021-03-07 05:10:17 -0500

    Created function to output training data in MuseGAN format (customizable length, normalized to middle C, transposable, can duplicate number of tracks, can merge multiple tracks into 1, standardizes tempo)

--- explore_track_df.py
+++ explore_track_df.py
@@ -1,7 +1,16 @@
 import os
+import time 
 import numpy as np
 import pandas as pd
 
 
-os.chdir(r'C:\Users\Darth\Desktop\CSC2506_Project')
-d = pd.read_json('C:\\Users\\Darth\\Desktop\\CSC2506_Project\\Raw Data\\track_df.json')
+os.chdir("/Users/sorensabet/Desktop/Master's Coursework/CSC2506_Project")
+
+song_df = pd.read_json('Raw Data/song_df.json')
+track_df = pd.read_json('Raw Data/track_df.json')
+msgs_df = pd.read_json('Raw Data/msgs_df.json')
+
+# song_df.to_hdf('Raw Data/song_df.h5', key='data')
+# track_df.to_hdf('Raw Data/track_df.h5', key='data')
+# msgs_df.to_hdf('Raw Data/msgs_df.h5', key='data')
+
